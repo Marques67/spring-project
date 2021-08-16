55eb8e1b1ebc7a88e35c15b6a02f963edfaecb5e
Lucas Marques <lucasmarquesff@hotmail.com>
2021-08-16 17:49:27 -0300

    Service layer, component registration

---
+++ src/main/java/br/lucas/springproject/Spring/Project/services/UserService.java
@@ -0,0 +1,25 @@
+package br.lucas.springproject.Spring.Project.services;
+
+import br.lucas.springproject.Spring.Project.entities.User;
+import br.lucas.springproject.Spring.Project.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
