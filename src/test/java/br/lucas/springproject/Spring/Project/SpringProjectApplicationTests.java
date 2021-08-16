fb2399b3d875546f35339661acfdfc30678d7536
Lucas Marques <lucasmarquesff@hotmail.com>
2021-08-16 15:31:13 -0300

    Project created

---
+++ src/test/java/br/lucas/springproject/Spring/Project/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lucas.springproject.Spring.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
