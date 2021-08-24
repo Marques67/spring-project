99fb140cac3ec0c2072b9e3b84af9a585eb02c53
Lucas Marques <lucasmarquesff@hotmail.com>
2021-08-24 17:56:36 -0300

    Subtotal & Total methods

--- src/main/java/br/lucas/springproject/Spring/Project/entities/Order.java
+++ src/main/java/br/lucas/springproject/Spring/Project/entities/Order.java
@@ -88,6 +88,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum = sum + x.getSubtotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
